c5fc7401da09aef1ee8a4be1b62cd5f21f2d776d
shenriquezs <shenriquezdsw@gmail.com>
2024-02-29 17:04:19 -0300

    se agrega autentication register login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Param, Post } from '@nestjs/common';
 import {AuthService} from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -16,7 +17,8 @@ export class AuthController {
     }
 
     @Post('login')
-    login(){
-        return this.authService.login();
+    login(@Body() loginDto: LoginDto )
+    {
+        return this.authService.login(loginDto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
+
+import * as bcryptjs from 'bcryptjs';
+import {LoginDto} from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
 
 constructor(
-    private readonly userService: UsersService,
+    private readonly usersService: UsersService,
 ){}
 
-    async register(registerDto : RegisterDto){
-        return await this.userService.create(registerDto);
+    async register({name, email, password} : RegisterDto){
+        const user= await this.usersService.findOneByEmail(email);
+
+        if(user){
+            throw new BadRequestException("User already exists");
+        }
+        return await this.usersService.create({name, email, password: await bcryptjs.hash(password, 10)});
     }
 
    
-    login(){
-        return 'login';
+    async login({email, password}: LoginDto){
+
+        const user= await this.usersService.findOneByEmail(email);
+
+        if(!user){
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        const isPasswordValid= await bcryptjs.compare(password, user.password);
+
+        if(!isPasswordValid){
+            throw new UnauthorizedException('Password is wrong');
+        }
+        return user;
     }
 
 }
